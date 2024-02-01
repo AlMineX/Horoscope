12db16505521c656d76dcd2f84320a81bc88ea97
AlMineX <157691484+AlMineX@users.noreply.github.com>
2024-02-01 11:53:30 +0100

    First commit

---
+++ models/model.php
@@ -0,0 +1,15 @@
+<?php
+$signe=array(
+    'Bélier'=>'Vous devez faire les efforts les plus sérieux pour aquérir un bon niveau.',
+    'Taureau'=>'Une de vos plus grande calités est la capacité de saisir des théories',
+    'Gémeaux'=>'De temps à autre, admettez que vous n\'êtes pas le seul à raisonner avec logique.',
+    'Cancer'=>'La rapidité de votre intelligencerisque de vous entraîner à négliger certains détails.',
+    'Lion'=>'Un effort vers la patience et la percévérance sera pour vous grandement payant.',
+    'Vierge'=>'Faites l\'effort de vous concentrer.',
+    'Balence'=>'Vous avez une forte tendance à choisir les solutions astucieuses.',
+    'Scorpion'=>'Renoncez à imposer vos propres données et appliquez les méthodes apprises.',
+    'Sagitaire'=>'Pardonner avec aisance et sans ambiguité, mais acceptez aucun extrême.',
+    'Capricorne'=>'Une certaine facilité ne doit pas vous détourner de votre objectif.',
+    'Verseau'=>'Ce travail vous demandera beaucoup d\'ingéniorité.',
+    'Poussons'=>'Votre espris vif et limpide facilite la résolution des problèmes',
+);
---
+++ views/choix.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="">
+        <select name="liste" id="">
+            <option value="">Bélier</option>
+            <option value="">Taureau</option>
+            <option value="">Gémeaux</option>
+            <option value="">Cancer</option>
+            <option value="">Lion</option>
+            <option value="">Vierge</option>
+            <option value="">Balence</option>
+            <option value="">Scorpion</option>
+            <option value="">Sagitaire</option>
+            <option value="">Capricorne</option>
+            <option value="">Verseau</option>
+            <option value="">Poissons</option>
+        </select>
+        <input type="submit" value="Envoyer">
+    </form>
+</body>
+</html>
